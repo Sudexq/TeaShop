6c94f22807619686192c04d23d909c491ca90d13
SudenurTilla <121225010+Sudexq@users.noreply.github.com>
2024-08-19 18:02:52 +0300

    navbar update, db.json for products and initialize products page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import {NavLink} from "react-router-dom"
-
+import logo from "../photos/logo.png"
 export const Navbar = () => {
   return (
     <nav className='navbar'>
+        <NavLink className="logo" to="/"><img src={logo} alt='logo'/></NavLink>
         <NavLink to="/">Home</NavLink>
         <NavLink to="/products">Products</NavLink>
     </nav>
