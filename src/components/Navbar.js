4f0ee351cb278e39d844549ad97e7d52d1e4de02
SudenurTilla <121225010+Sudexq@users.noreply.github.com>
2024-09-08 15:18:07 +0300

    updates
    navbar updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,44 @@
-import React from 'react'
-import {NavLink} from "react-router-dom"
-import logo from "../photos/logo.png"
-import ShoppingCartIcon from './ShoppingCartIcon'
+import React, {useState}from "react";
+import { NavLink } from "react-router-dom";
+import logo from "../photos/logo.png";
+import ShoppingCartIcon from "./ShoppingCartIcon";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // FontAwesome'dan ikonları ekliyoruz
+import { faBars } from "@fortawesome/free-solid-svg-icons"; // Hamburger menü ikonu
+
 export const Navbar = () => {
+  const [isResponsive, setIsResponsive] = useState(false); // Responsive state'i
+
+  const toggleMenu = () => {
+    setIsResponsive(!isResponsive); // Tıkladıkça menüyü açıp kapatır
+  };
+
   return (
-    <nav className='navbar'>
-        <NavLink className="logo" to="/"><img src={logo} alt='logo'/></NavLink>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/products">Products</NavLink>
-        <NavLink className="cart" to="/cart"><ShoppingCartIcon/></NavLink>
+    <nav className={`navbar ${isResponsive ? "navbar-responsive" : ""}`}>
+      <div className="navbar-content">
+        <NavLink className="logo" to="/">
+          <img src={logo} alt="logo" />
+        </NavLink>
+
+        <div
+          className={`nav-links ${isResponsive ? "nav-links-responsive" : ""}`}
+        >
+          <NavLink to="/" onClick={toggleMenu}>
+            Home
+          </NavLink>
+          <NavLink to="/products" onClick={toggleMenu}>
+            Products
+          </NavLink>
+        </div>
+
+        <NavLink className="cart" to="/cart">
+          <ShoppingCartIcon />
+        </NavLink>
+
+        {/* Font Awesome Hamburger Menüsü */}
+        <div className="hamburger" onClick={toggleMenu}>
+          <FontAwesomeIcon icon={faBars} />
+        </div>
+      </div>
     </nav>
-  )
-}
+  );
+};
